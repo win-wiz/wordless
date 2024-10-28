4219204f2306fdcf668f2e2b54651884cae549b3
tjsglion <465264938@qq.com>
2024-10-28 10:37:21 +0800

    增加icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,9 @@
+import Image from "next/image";
+import logo from "@/../public/wordless.png";
+
 export default function Header() {
   return <div className="h-16 shadow-md sticky top-0 z-50 border-b-2 border-slate-500 text-white flex items-center justify-center">
+    <Image src={logo} alt="logo" width={32} height={32} className="mr-2" />
     <h1 className="text-2xl font-bold bg-gradient-to-r from-white to-zinc-500 bg-clip-text text-transparent">Wordless</h1>
   </div>;
 }
